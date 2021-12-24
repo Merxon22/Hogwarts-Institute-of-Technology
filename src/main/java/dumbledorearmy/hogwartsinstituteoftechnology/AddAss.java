5f7dee9d228cdf01e091386b93459ef0fe79faab
Eurethia <3366841721@qq.com>
2021-12-24 12:06:13 +0800

    nothing to say

---
+++ src/main/java/dumbledorearmy/hogwartsinstituteoftechnology/AddAss.java
@@ -0,0 +1,14 @@
+package dumbledorearmy.hogwartsinstituteoftechnology;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "AddAss", value = "/AddAss")
+public class AddAss extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
