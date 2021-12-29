3f4a102f8f271566923845e8435a00ba6661f75d
Eurethia <3366841721@qq.com>
2021-12-29 21:31:04 +0800

    nothing to say

--- src/main/java/dumbledorearmy/hogwartsinstituteoftechnology/ViewSche.java
+++ src/main/java/dumbledorearmy/hogwartsinstituteoftechnology/ViewSche.java
@@ -4,16 +4,192 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLOutput;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @WebServlet(name = "ViewSche", value = "/ViewSche")
 public class ViewSche extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //现在有老师的邮箱，通过邮箱得到老师的id，这个和classinfo的id一连起来，就可以获得时间了，接下来就是排版了
 
+
+        HashMap<String, Integer> mapWeek = new HashMap<>();
+        HashMap<String, Integer> mapClass = new HashMap<>();
+        mapWeek.put("Monday", 0);
+        mapWeek.put("Tuesday", 1);
+        mapWeek.put("Wednesday", 2);
+        mapWeek.put("Thursday", 3);
+        mapWeek.put("Friday", 4);
+        mapClass.put("8:30-9:25", 0);
+        mapClass.put("9:30-10:25", 1);
+        mapClass.put("10:30-11:25", 2);
+        mapClass.put("11:30-12:25", 3);
+        mapClass.put("13:15-14:10", 4);
+        mapClass.put("14:15-15:10", 5);
+        mapClass.put("15:15-16:10", 6);
+
+        try{
+            ScheduleHelper.initialize();
+            HashMap<String, Integer> map5= ScheduleHelper.getMapWeek();
+            HashMap<String, Integer> map7= ScheduleHelper.getMapClass();
+
+            PrintWriter out = response.getWriter();
+            response.setContentType("text/html");
+            out.println("<html>");
+
+            Cookie[] cookies = request.getCookies();
+            String em = cookies[0].getValue();
+            int id = 0;
+            Connection con = Provider.GetConn();
+            Statement stmt = con.createStatement();
+            String query = "select * from teacher where email='" + em + "';";
+
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()){
+                id = rs.getInt("id");
+            }
+            System.out.println(id);
+            query = "select classinfo.time from classinfo" +
+                    " join teacher on teacher_id = teacher.id where teacher_id=" + id;
+
+            rs = stmt.executeQuery(query);
+            String[][] week = new String[5][7];//对应一整周，5天
+            while(rs.next()) { //默认我们后期会一个老师教很多课，但只有1
+                String time = rs.getString("time");
+                String[] times = time.split("<br>");
+                for (String aTime: times){
+                    String[] whole = aTime.split(" ");
+
+                    System.out.println(whole[0]);
+                    System.out.println(whole[1]);
+
+                    System.out.println(mapWeek.get(whole[0]));
+                    System.out.println(mapClass.get(whole[1]));
+
+                    int weekday = mapWeek.get(whole[0]);
+                    int period = mapClass.get(whole[1]);
+                    week[weekday][period] = rs.getString("subject");
+                }
+            }
+            out.println("<table>\n" +
+                    "    <thead>\n" +
+                    "    <tr>\n" +
+                    "        <th>Time/Weekday</th>\n" +
+                    "        <th>Monday</th>\n" +
+                    "        <th>Tuesday</th>\n" +
+                    "        <th>Wednesday</th>\n" +
+                    "        <th>Thursday</th>\n" +
+                    "        <th>Friday</th>\n" +
+                    "    </tr>\n" +
+                    "    </thead>\n" +
+                    "    <tbody>\n" +
+                    "    <tr>\n" +
+                    "        <td>8:30-9:25</td>");
+
+            for (int per = 0; per < 5; per++){
+                if (!week[per][0].equals(null)){
+                    out.println("<td>" + week[0][per] + "</td>");
+                }
+                else{
+                    out.println("<td>Free Period</td>");
+                }
+            }
+
+            out.print("</tr>\n" +
+                    "    <tr>\n" +
+                    "        <td>9:30-10:25</td>");
+
+            for (int per = 0; per < 5; per++){
+                if (!week[per][1].equals(null)){
+                    out.println("<td>" + week[0][per] + "</td>");
+                }
+                else{
+                    out.println("<td>Free Period</td>");
+                }
+            }
+
+            out.println("</tr>\n" +
+                    "    <tr>\n" +
+                    "        <td>10:30-11:25</td>");
+
+            for (int per = 0; per < 5; per++){
+                if (!week[per][2].equals(null)){
+                    out.println("<td>" + week[0][per] + "</td>");
+                }
+                else{
+                    out.println("<td>Free Period</td>");
+                }
+            }
+            out.println("</tr>\n" +
+                    "    <tr>\n" +
+                    "        <td>11:30-12:25</td>");
+
+            for (int per = 0; per < 5; per++){
+                if (!week[per][3].equals(null)){
+                    out.println("<td>" + week[0][per] + "</td>");
+                }
+                else{
+                    out.println("<td>Free Period</td>");
+                }
+            }
+
+            out.println("</tr>\n" +
+                    "    <tr>\n" +
+                    "        <td>Lunch Break</td>\n" +
+                    "        <td id = \"spec\" colspan=\"4\">Recharge Life Support System</td>\n" +
+                    "    </tr>\n" +
+                    "    <tr>\n" +
+                    "        <td>13:15-14:10</td>");
+
+            for (int per = 0; per < 5; per++){
+                if (!week[per][4].equals(null)){
+                    out.println("<td>" + week[0][per] + "</td>");
+                }
+                else{
+                    out.println("<td>Free Period</td>");
+                }
+            }
+            out.println("</tr>\n" +
+                    "    <tr>\n" +
+                    "        <td>14:15-15:10</td>");
+
+            for (int per = 0; per < 5; per++){
+                if (!week[per][5].equals(null)){
+                    out.println("<td>" + week[0][per] + "</td>");
+                }
+                else{
+                    out.println("<td>Free Period</td>");
+                }
+            }
+
+            out.println("</tr>\n" +
+                    "    <tr>\n" +
+                    "        <td>15:15-16:10</td>");
+
+            for (int per = 0; per < 5; per++){
+                if (!week[per][6].equals(null)){
+                    out.println("<td>" + week[0][per] + "</td>");
+                }
+                else{
+                    out.println("<td>Free Period</td>");
+                }
+            }
+            out.println("</tr>\n" +
+                    "    </tbody>\n" +
+                    "</table>");
+
+            out.println("<html>");
+        con.close();
+
+        }catch (Exception exe){
+            System.out.println(exe);
+        }
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 }
