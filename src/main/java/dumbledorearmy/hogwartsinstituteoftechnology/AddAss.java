5b0a9f341fb5c390c3ba54f8627de442f181707b
Eurethia <3366841721@qq.com>
2022-01-03 23:33:27 +0800

    啊这

--- src/main/java/dumbledorearmy/hogwartsinstituteoftechnology/AddAss.java
+++ src/main/java/dumbledorearmy/hogwartsinstituteoftechnology/AddAss.java
@@ -19,16 +19,23 @@ public class AddAss extends HttpServlet {
             String allAss[] = request.getParameter("ass").split("\n");
             String que;
             Statement stmt = con.createStatement();
+            int count = 0;
             for (String as: allAss){
+
                 String sep[] = as.split("/");
                 String cN = sep[0];
                 String className = cN.substring(0, 1).toUpperCase() + cN.substring(1, cN.length());
                 String assName = sep[1];
+                if (count != allAss.length - 1){
+                    assName = assName.substring(0, assName.length() - 1);
+                }
+
 
                 System.out.println(assName);
                 que = "ALTER TABLE " + className + "\n" +
                         "ADD COLUMN `" + assName + "` varchar(64) DEFAULT 'N/A';";
                 stmt.executeUpdate(que);
+                count++;
             }
             RequestDispatcher rd = request.getRequestDispatcher("Teacher.jsp");
 
